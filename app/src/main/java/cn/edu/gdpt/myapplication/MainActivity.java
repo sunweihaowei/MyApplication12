95d9d9393c2967e4e5672754258d086ff1825ec0
admin <1096187594@qq.com>
2019-05-23 15:43:26 +0800

    listView contextMenu

--- app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
@@ -16,7 +16,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.SimpleTimeZone;
-
+/*
+* 总结：listView与ContextMenu
+*
+*
+*
+*
+* */
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv;
@@ -43,20 +49,22 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         lv = (ListView) findViewById(R.id.lv);
     }
-
+    //创建ContextMenu，
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
+        //创建适配器上下文信息，得到这个类的方法info.position，即为得到长按项，让其等于一个int类型的值，这里为item_id
         AdapterView.AdapterContextMenuInfo info= (AdapterView.AdapterContextMenuInfo) menuInfo;
-
         item_id=info.position;
+        //菜单加载器，来加载菜单
         MenuInflater menuInflater=new MenuInflater(MainActivity.this);
         menuInflater.inflate(R.menu.menu,menu);
     }
-
+    //上下文的选择项
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()){
+            //这里的item_id为刚刚定义的一个值，即为我们想得到的项
             case R.id.menu_delete:list.remove(item_id);
             simpleAdapter.notifyDataSetChanged();
             break;
